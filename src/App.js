90cb45bf29e637a672e87687c057ab805fd7a973
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-04 09:10:19 -0500

    added style to expense item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h1>React Journey Initialized</h1>
-	    <p>DOM test DOM test DOM test!!!!</p>
+	  <ExpenseItem></ExpenseItem>
     </div>
-	  
-	  
   );
 }
 
